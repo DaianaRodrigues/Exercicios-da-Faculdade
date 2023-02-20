4247b336c51f37956756dd8558cfa8371a2c2d9d
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2023-02-19 22:14:24 -0300

    Atualização 2

--- Faculdade/Semana2.py
+++ Faculdade/Semana2.py
@@ -29,10 +29,71 @@ print(absoluto)
 preco1 = 34.99
 preco2 = 29.95
 preco3 = 31.50
-min(preco1, preco2, preco3)
 print(min(preco1, preco2, preco3))
 
 # Exercicio 2.2
 # A soma de 2 e 2 é menor que 4
+res = 2 + 2 < 4
+print(res)
 
+# O valor de 7 // 3 é igual a 1 + 1.
+res1 = 7 // 3 == 1 + 1
+print(res1)
 
+# A soma de 3 ao quadrado e 4 ao quadrado é igual a 25.
+res3 = 3**2 + 4**2 == 25
+print(res3)
+
+# A soma de 2, 4 e 6 é maior que 12.
+res4 = 2 + 4 + 6 > 12
+print(res4)
+
+# 1387 é divisível por 19.
+res8 = 1387 % 19 == 0
+print(res8)
+
+# 31 é par.
+res5 = 31 % 2 == 0
+print(res5)
+
+# O preço mais baixo dentre R$ 34,99, R$ 29,95 e R$ 31,50 é menor que R$ 30,00.
+res6 = min(preco1, preco2, preco3) < 30.00
+print(res6)
+
+# Exercicio 2.3
+# Atribua o valor inteiro 3 à variável a.
+a = 3
+
+# Atribua 4 à variável b.
+b = 4
+
+# Atribua à variável c o valor da expressão a * a + b * b.
+c = a * a + b * b
+print(c)
+
+# Exercicio 2.4
+s1 = 'ant '
+s2 = 'bat '
+s3 = 'cod '
+# 'ant bat cod'
+print(s1 + s2 + s3)
+
+# ant ant ant ant ant ant ant ant ant ant
+print(s1 * 10)
+
+# 'ant bat bat cod cod cod'
+print(s1 + s2 * 2 + s3 * 3)
+
+# ant bat ant bat ant bat ant bat ant bat ant bat ant bat
+print((s1 + s2) * 7)
+
+# batbatcod batbatcod batbatcod batbatcod batbatcod
+print((s2 + s2 + s3) * 5)
+
+# Exercicio 2.5
+s = '0123456789'
+print(s[0])
+print(s[1])
+print(s[6])
+print(s[8])
+print(s[9])
