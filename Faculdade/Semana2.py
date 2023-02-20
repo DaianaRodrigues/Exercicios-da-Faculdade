2481940534eb361b4294e3dc1cb9c5ed1b12f1a8
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2023-02-19 21:20:27 -0300

    Atualização

---
+++ Faculdade/Semana2.py
@@ -0,0 +1,38 @@
+#Excicio 2.1
+# A soma dos 5 primeiros inteiros positivos.
+soma = (1 + 2 + 3 + 4 + 5)
+print(soma)
+
+# A idade média de Sara (idade 65), Fátima (idade 56) e Mark (idade 45)
+sara = 65
+fatima = 56
+mark = 45
+media = (sara + fatima + mark) / 3
+print(media)
+
+#2 à 10 potência
+potencia = 2**10
+print(potencia)
+
+# O número de vezes que 73 cabe em 403.
+divisao = 403 // 73
+
+# O resto de quando 403 é dividido por 73.
+resto = 403 % 37
+print(resto)
+
+# O valor absoluto da distância entre a altura de Sara (54 polegadas) e a altura de Mark (57 polegadas).
+absoluto = (sara - mark)
+print(absoluto)
+
+# O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.
+preco1 = 34.99
+preco2 = 29.95
+preco3 = 31.50
+min(preco1, preco2, preco3)
+print(min(preco1, preco2, preco3))
+
+# Exercicio 2.2
+# A soma de 2 e 2 é menor que 4
+
+
