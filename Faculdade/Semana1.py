dc47ef662f540ec669bd3bdf84705de76d78431c
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2023-03-11 22:39:51 -0300

    Atualização 4

--- Faculdade/Semana1.py
+++ Faculdade/Semana1.py
@@ -23,5 +23,10 @@ else:
 #Desenvolver os diagramas de blocos e as codificações em português estruturado usando laço incondicional
 # (para) do seguinte problema: Construir um programa que apresente a soma dos cem primeiros
 # números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+soma = 0
+for i in range(1, 101):
+    soma+= i
+
+print("O resultado da soma é", soma)
 
 
---
+++ Faculdade/Semana3.py
@@ -0,0 +1,7 @@
+# Exercício 2.6
+#escreva duas expressões Python que são avaliadas,
+#respectivamente, como a primeiro e a última palavras em palavras, na ordem do dicionário.
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+print(sorted(palavras[0]))
+print(sorted(palavras[4]))
+
