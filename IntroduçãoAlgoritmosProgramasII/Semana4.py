497f4eb32d46be3a2fa65d0e8fa671e951ecae83
DaianaRodrigues <daiana-rodrigues-2007@hotmail.com>
2023-05-18 00:38:55 -0300

    Atualização 4.1

--- IntroduçãoAlgoritmosProgramasII/Semana4.py
+++ IntroduçãoAlgoritmosProgramasII/Semana4.py
@@ -17,11 +17,12 @@ def rlookup(agenda):
     '(321)908-76-54':['Hans', 'Castorp']}
 
     while True:
-        numero = input('Digite número do telefone no formato(xxx) xxx - xx - xx: ')
+        numero = input('Digite número do telefone no formato(xxx)xxx-xx-xx: ')
         if numero in agenda:
              print(agenda[numero])
         else:
              print('O número informado não está em uso.')
+
     return
 
 #Exercicio 6.4
@@ -40,4 +41,34 @@ def contaPalavra(texto):
         else:
             print('{:8} aparece {} vezes.'.format(palavra, contadores[palavra]))
     return
+print(contaPalavra('Oi Mundo'))
 
+#Exercicio 6.5
+def lookup(agenda):
+    #implementa serviço de agenda interativo usando o dicionário de agenda como entrada
+    agenda = {('Anna', 'Karenina'): '(123)456-78-90', ('Yu', 'Tsun'): '(901)234-56-78', ('Hans', 'Castorp'):'(321)908-76-54)'}
+    while True:
+        nome = input('Digite o nome ')
+        sobrenome = input('Digite o sobrenome ')
+        pessoa = (nome, sobrenome)# constrói a chave
+        if pessoa in agenda:# se a chave estiver no dicionário
+            print(agenda[pessoa])# mostra o valor
+        else: # se chave ausente do dicionário
+            print('O nome informado não é conhecido.')
+
+    return
+
+
+#Exercicio 6.6
+def sync(agendas):
+    #retorna a união de conjuntos em agendas'
+    res = set() # inicializa o acumulador
+
+    for agenda in agendas:
+        res = res | agenda # acumula agenda em res
+
+    return res
+agenda1 = ['4738', '9389']
+agenda2 = ['02834', '9380947']
+agendas = [agenda1, agenda2]
+print(agendas)
